c6ef520af530d908c6a4dfee4a895dbd5bb1ee86
fernandomv3 <fernandomv3@gmail.com>
2026-04-10 16:54:25 -0500

    deskew not working

--- preprocessing.py
+++ preprocessing.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import zxingcpp
 from cv2.typing import MatLike
+from PIL import Image
 
 from form import BarcodesSegment, Form
 
@@ -17,7 +18,7 @@ def preprocess_image_barcodes(config: Form, src_img: MatLike) -> MatLike:
     bottom_left = None
     top_right = None
 
-    blur_img = cv2.GaussianBlur(src_img, ksize=(5, 5), sigmaX=0)
+    blur_img = cv2.GaussianBlur(src_img, ksize=(3, 3), sigmaX=0)
     barcodes = zxingcpp.read_barcodes(blur_img)
     for code in barcodes:
         if code.text == segment.bottom_left.text:
@@ -29,9 +30,8 @@ def preprocess_image_barcodes(config: Form, src_img: MatLike) -> MatLike:
 
     if (bottom_left is None) or (top_right is None):
         raise RuntimeError("Segment barcodes not found")
-
-    skew = (bottom_left.orientation + top_right.orientation) / 2
-
+    skew = 0
+    Image.fromarray(blur_img[top_right.position.top_left.y :, :]).save("debug.png")
     rot_mat = cv2.getRotationMatrix2D((0, 0), -skew, 1)
 
     img = cv2.warpAffine(
@@ -40,10 +40,10 @@ def preprocess_image_barcodes(config: Form, src_img: MatLike) -> MatLike:
         (src_img.shape[1], src_img.shape[0]),
         borderMode=cv2.BORDER_CONSTANT,
         borderValue=0,
-        flags=cv2.INTER_CUBIC,
+        flags=cv2.INTER_LANCZOS4,
     )
 
-    blur_img = cv2.GaussianBlur(img, ksize=(5, 5), sigmaX=0)
+    blur_img = cv2.GaussianBlur(img, ksize=(3, 3), sigmaX=0)
     barcodes = zxingcpp.read_barcodes(blur_img)
     for code in barcodes:
         if code.text == segment.bottom_left.text:
@@ -61,17 +61,15 @@ def preprocess_image_barcodes(config: Form, src_img: MatLike) -> MatLike:
 
     segment_width = tr.x - bl.x
     segment_height = bl.y - tr.y
-
     scale_factor = np.array(segment.size) / np.array([segment_width, segment_height])
     position = np.array([bl.x, tr.y])
     offset = np.array(segment.position) - (position * scale_factor)
-
     img = cv2.resize(
         img,
         None,
         fx=scale_factor[0],
         fy=scale_factor[1],
-        interpolation=cv2.INTER_LANCZOS4,
+        interpolation=cv2.INTER_LINEAR,
     )
 
     tmat = np.array([[1, 0, offset[0]], [0, 1, offset[1]]], dtype=np.float32)
