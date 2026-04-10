f07dd06b5dd6a498c1cf987457fb0c9cb1c6bac4
fernandomv3 <fernandomv3@gmail.com>
2026-04-10 12:04:46 -0500

    preprocessing done on barcoded images

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import sys
 
+import numpy as np
+import pdf2image
+import tqdm
+from PIL import Image
+
 from form import Form
+from preprocessing import preprocess_image_barcodes
 
 
 def main(args):
     config_file = args[1]
-    config = None
+    fname = args[2]
     with open(config_file) as f:
         json_config = f.read()
         config = Form.model_validate_json(json_config)
-    print(config)
+
+    images = pdf2image.convert_from_path(fname)
+    for i, img in enumerate(tqdm.tqdm(images)):
+        res = preprocess_image_barcodes(config, np.array(img))
+        Image.fromarray(res).save(f"{i}.png")
 
 
 if __name__ == "__main__":
---
+++ preprocessing.py
@@ -0,0 +1,91 @@
+import cv2
+import numpy as np
+import zxingcpp
+from cv2.typing import MatLike
+
+from form import BarcodesSegment, Form
+
+
+def preprocess_image_barcodes(config: Form, src_img: MatLike) -> MatLike:
+    for s in config.segments:
+        if isinstance(s, BarcodesSegment):
+            segment = s
+            break
+    else:
+        raise RuntimeError("No barcode segments in config")
+
+    bottom_left = None
+    top_right = None
+
+    blur_img = cv2.GaussianBlur(src_img, ksize=(5, 5), sigmaX=0)
+    barcodes = zxingcpp.read_barcodes(blur_img)
+    for code in barcodes:
+        if code.text == segment.bottom_left.text:
+            bottom_left = code
+        elif code.text == segment.top_right.text:
+            top_right = code
+        else:
+            continue
+
+    if (bottom_left is None) or (top_right is None):
+        raise RuntimeError("Segment barcodes not found")
+
+    skew = (bottom_left.orientation + top_right.orientation) / 2
+
+    rot_mat = cv2.getRotationMatrix2D((0, 0), -skew, 1)
+
+    img = cv2.warpAffine(
+        src_img,
+        rot_mat,
+        (src_img.shape[1], src_img.shape[0]),
+        borderMode=cv2.BORDER_CONSTANT,
+        borderValue=0,
+        flags=cv2.INTER_CUBIC,
+    )
+
+    blur_img = cv2.GaussianBlur(img, ksize=(5, 5), sigmaX=0)
+    barcodes = zxingcpp.read_barcodes(blur_img)
+    for code in barcodes:
+        if code.text == segment.bottom_left.text:
+            bottom_left = code
+        elif code.text == segment.top_right.text:
+            top_right = code
+        else:
+            continue
+
+    if (bottom_left is None) or (top_right is None):
+        raise RuntimeError("Segment barcodes not found")
+
+    bl = bottom_left.position.bottom_left
+    tr = top_right.position.top_right
+
+    segment_width = tr.x - bl.x
+    segment_height = bl.y - tr.y
+
+    scale_factor = np.array(segment.size) / np.array([segment_width, segment_height])
+    position = np.array([bl.x, tr.y])
+    offset = np.array(segment.position) - (position * scale_factor)
+
+    img = cv2.resize(
+        img,
+        None,
+        fx=scale_factor[0],
+        fy=scale_factor[1],
+        interpolation=cv2.INTER_LANCZOS4,
+    )
+
+    tmat = np.array([[1, 0, offset[0]], [0, 1, offset[1]]], dtype=np.float32)
+    img = cv2.warpAffine(
+        img,
+        tmat,
+        dsize=(img.shape[1], img.shape[0]),
+        borderMode=cv2.BORDER_CONSTANT,
+        borderValue=(255, 255, 255, 255),
+    )
+
+    img = cv2.copyMakeBorder(
+        img, 0, 50, 0, 50, cv2.BORDER_CONSTANT, value=(255, 255, 255, 255)
+    )
+    img = img[0 : config.page_size[1], 0 : config.page_size[0]]
+
+    return img
