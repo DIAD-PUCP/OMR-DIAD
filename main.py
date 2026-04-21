ba53084291b77e219ab8691f66ecbd6f582ec486
fernandomv3 <fernandomv3@gmail.com>
2026-04-21 11:15:59 -0500

    Using Path as type for the files and added outdir and debug dir as options

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from multiprocessing import Pool
+from pathlib import Path
 from typing import Annotated, Optional
 
 import numpy as np
@@ -26,16 +27,18 @@ def proc_img(data) -> Optional[list[str]]:
 
 @app.command()
 def main(
-    config_file: Annotated[str, typer.Argument()],
-    fname: Annotated[str, typer.Argument()],
+    fname: Annotated[Path, typer.Argument()],
+    config_file: Annotated[Path, typer.Option()],
     single_process: Annotated[bool, typer.Option()] = False,
+    out_dir: Annotated[Path, typer.Option()] = Path("outputs"),
+    debug_dir: Annotated[Path, typer.Option()] = Path("debug"),
 ):
     with open(config_file) as f:
         json_config = f.read()
         config = Form.model_validate_json(json_config)
     n_threads = os.cpu_count() or 1
     images = [
-        (config, np.array(img), "outputs", "debug")
+        (config, np.array(img), out_dir, debug_dir)
         for img in pdf2image.convert_from_path(
             fname, fmt="jpeg", thread_count=n_threads
         )
