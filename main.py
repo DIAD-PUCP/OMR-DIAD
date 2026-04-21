7fc5b4af6efbc37a6401a32ff9fac00d80f87bc3
fernandomv3 <fernandomv3@gmail.com>
2026-04-21 10:57:17 -0500

    cli option to force single process

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import sys
 from multiprocessing import Pool
 from typing import Annotated, Optional
@@ -27,17 +28,27 @@ def proc_img(data) -> Optional[list[str]]:
 def main(
     config_file: Annotated[str, typer.Argument()],
     fname: Annotated[str, typer.Argument()],
+    single_process: Annotated[bool, typer.Option()] = False,
 ):
     with open(config_file) as f:
         json_config = f.read()
         config = Form.model_validate_json(json_config)
-
+    n_threads = os.cpu_count() or 1
     images = [
         (config, np.array(img), "outputs", "debug")
-        for img in pdf2image.convert_from_path(fname, fmt="jpeg", thread_count=8)
+        for img in pdf2image.convert_from_path(
+            fname, fmt="jpeg", thread_count=n_threads
+        )
     ]
-    with Pool() as p:
-        results = list(tqdm.tqdm(p.imap(proc_img, images), total=len(images)))
+
+    if not single_process:
+        with Pool() as p:
+            results = list(tqdm.tqdm(p.imap(proc_img, images), total=len(images)))
+    else:
+        results = []
+        for data in tqdm.tqdm(images):
+            results.append(proc_img(data))
+
     r = []
     for ficha in results:
         if ficha is None:
