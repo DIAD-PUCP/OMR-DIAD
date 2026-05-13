583598729d8e68672a96b7057ac8541e585205e2
fernandomv3 <fernandomv3@gmail.com>
2026-05-13 17:08:50 -0500

    Added test for config header generation

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -21,5 +21,23 @@ def test_config_load(subtests):
             assert isinstance(config.form_id, Barcode)
 
 
+def test_config_header(subtests):
+    with open("../sample_configs/config.json") as f:
+        json_config = f.read()
+        config = Form.model_validate_json(json_config)
+        with subtests.test("DIAD config header"):
+            header = config.get_header()
+            assert header == ["ID"] + [f"item{i}" for i in range(1, 77)]
+
+        for i, seg in enumerate(config.segments):
+            for j, _ in enumerate(seg.item_blocks):
+                config.segments[i].item_blocks[j].labels = None
+        with subtests.test("DIAD config header without labels"):
+            header = config.get_header()
+            assert header == ["ID"] + [f"item{i}" for i in range(1, 31)] * 2 + [
+                f"item{i}" for i in range(1, 17)
+            ]
+
+
 def test_form_diad():
     assert True
