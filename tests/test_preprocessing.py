668485274c81a65baa98e53be5fb643e092af8fe
fernandomv3 <fernandomv3@gmail.com>
2026-05-13 16:47:14 -0500

    Test for loading configs

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,3 +1,25 @@
-def test_preprocessing():
-    # find_skew()
+from src.form import Barcode, Form, ItemBlock
+
+
+def test_config_load(subtests):
+    with open("../sample_configs/config.json") as f:
+        json_config = f.read()
+        with subtests.test("DIAD config load"):
+            config = Form.model_validate_json(json_config)
+            assert isinstance(config.form_id, ItemBlock)
+
+    with open("../sample_configs/config_formreturn_diad.json") as f:
+        json_config = f.read()
+        with subtests.test("DIAD FormReturn config load"):
+            config = Form.model_validate_json(json_config)
+            assert isinstance(config.form_id, Barcode)
+
+    with open("../sample_configs/config_formreturn.json") as f:
+        json_config = f.read()
+        with subtests.test("DIAD FormReturn sample config load"):
+            config = Form.model_validate_json(json_config)
+            assert isinstance(config.form_id, Barcode)
+
+
+def test_form_diad():
     assert True
