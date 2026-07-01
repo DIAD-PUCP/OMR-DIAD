743d426497b46476a807a138feb1098bdb9e261a
fernandomv3 <fernandomv3@gmail.com>
2026-06-30 22:58:07 -0500

    First verion of app

--- revisar.py
+++ revisar.py
@@ -12,19 +12,24 @@ def main():
             .set_index("EXAMEN")
             .loc[:, "item1":]
             .sort_index()
+            .fillna("")
         )
+        fr = fr.map(lambda x: "!!ERROR!!" if x.startswith("!!ERROR!!") else x)
         omr = (
             pd.read_csv(archivo_omr, dtype="str")
             .set_index("EXAMEN")
             .loc[:, "item1":]
             .sort_index()
+            .fillna("")
         )
-        diff = fr.compare(omr)
+        diff = fr.compare(omr, result_names=("FormReturn", "OMR"))
         num_diff = len(diff)
         st.text(f"Se encontraron {num_diff} fichas diferentes.")
-
-        for examen, rec in diff.iterrows():
-            st.write(rec)
+        ficha = st.selectbox("Seleccione una ficha:", options=diff.index.unique())
+        d = pd.DataFrame(diff.loc[ficha]).dropna()
+        for i in d.index.unique(0):
+            st.markdown(f"### {i}")
+            st.dataframe(d.loc[i].T, width="content")
 
 
 if __name__ == "__main__":
