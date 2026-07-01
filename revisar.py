d7635ec1f30de6bf40c4da4a01c323381c06df3d
fernandomv3 <fernandomv3@gmail.com>
2026-07-01 11:59:24 -0500

    corrections are managed an exported

--- revisar.py
+++ revisar.py
@@ -1,3 +1,4 @@
+import csv
 import os
 from io import StringIO
 from pathlib import Path
@@ -23,9 +24,24 @@ def get_scans(scans_dir: Path) -> dict[str, Path]:
     return res
 
 
+def procesar_cambios(original):
+    df = original.copy()
+    cambios = st.session_state["correcciones"]
+    for ficha in cambios:
+        for item, rsp in cambios[ficha].items():
+            df.loc[df["EXAMEN"] == ficha, item] = rsp
+    return df.to_csv(index=None, quoting=csv.QUOTE_ALL)
+
+
 def main():
+    if "correcciones" not in st.session_state:
+        st.session_state["correcciones"] = {}
+    with st.sidebar:
+        st.write(st.session_state["correcciones"])
+        if st.button("Limpiar"):
+            st.session_state["correcciones"] = {}
     st.title("Revisión de fichas")
-    files = get_scans(Path("outputs"))
+    files = get_scans(Path("outputs/cepresimjun26/"))
     archivo_plantilla = st.file_uploader("Plantilla ficha")
     if archivo_plantilla:
         form_json = StringIO(archivo_plantilla.getvalue().decode("utf-8")).read()
@@ -34,13 +50,18 @@ def main():
     archivo_formreturn = st.file_uploader("Archivo formreturn")
     archivo_omr = st.file_uploader("Archivo OMR")
     if archivo_formreturn and archivo_omr:
-        fr = (
-            pd.read_csv(archivo_formreturn, dtype="str")
-            .set_index("EXAMEN")
-            .loc[:, "item1":]
-            .sort_index()
-            .fillna("")
-        )
+        fr = pd.read_csv(archivo_formreturn, dtype="str")
+        with st.sidebar:
+            if st.button("Procesar cambios"):
+                res = procesar_cambios(fr)
+                st.download_button(
+                    "Descargar",
+                    data=res.encode("utf-8"),
+                    file_name="resultado.csv",
+                    mime="text/csv",
+                )
+
+        fr = fr.set_index("EXAMEN").loc[:, "item1":].sort_index().fillna("")
         fr = fr.map(lambda x: "!!ERROR!!" if x.startswith("!!ERROR!!") else x)
         omr = (
             pd.read_csv(archivo_omr, dtype="str")
@@ -59,11 +80,18 @@ def main():
             img = np.array(Image.open(file))
         for i in d.index.unique(0):
             st.markdown(f"### {i}")
-            col1, col2 = st.columns(2, vertical_alignment="center")
+            col1, col2, col3, col4 = st.columns(4, vertical_alignment="center")
             col1.dataframe(d.loc[i].T, width="content")
             if positions:
                 x, y, w, h = positions[i]
                 col2.write(Image.fromarray(img[y : y + h, x : x + w, :]))
+            r = col3.text_input("res:", key=f"input_{ficha}_{i}", max_chars=8, width=60)
+            b = col4.button("Corregir", key=f"button_{ficha}_{i}")
+            if b:
+                if ficha not in st.session_state["correcciones"]:
+                    st.session_state["correcciones"][ficha] = {}
+                st.session_state["correcciones"][ficha][i] = r
+        st.write(Image.fromarray(img))
 
 
 if __name__ == "__main__":
