0e461536de0612377bab5ab838c0b4ea086907bd
fernandomv3 <fernandomv3@gmail.com>
2026-06-30 23:55:34 -0500

    image of conflicting item is shown

--- revisar.py
+++ revisar.py
@@ -1,13 +1,31 @@
+import os
 from io import StringIO
+from pathlib import Path
 
+import numpy as np
 import pandas as pd
 import streamlit as st
+from PIL import Image
 
 from src.form import Form
 
 
+def get_scans(scans_dir: Path) -> dict[str, Path]:
+    res = {}
+    files = (
+        Path(f"{scans_dir}/{file}")
+        for file in os.listdir(scans_dir)
+        if file.endswith(".png")
+    )
+    for file in files:
+        examen = file.name[:5]
+        res[examen] = file
+    return res
+
+
 def main():
     st.title("Revisión de fichas")
+    files = get_scans(Path("outputs"))
     archivo_plantilla = st.file_uploader("Plantilla ficha")
     if archivo_plantilla:
         form_json = StringIO(archivo_plantilla.getvalue().decode("utf-8")).read()
@@ -36,12 +54,16 @@ def main():
         st.text(f"Se encontraron {num_diff} fichas diferentes.")
         ficha = st.selectbox("Seleccione una ficha:", options=diff.index.unique())
         d = pd.DataFrame(diff.loc[ficha]).dropna()
+        if files:
+            file = files[ficha]
+            img = np.array(Image.open(file))
         for i in d.index.unique(0):
             st.markdown(f"### {i}")
-            st.dataframe(d.loc[i].T, width="content")
+            col1, col2 = st.columns(2, vertical_alignment="center")
+            col1.dataframe(d.loc[i].T, width="content")
             if positions:
                 x, y, w, h = positions[i]
-                st.write(x, y, w, h)
+                col2.write(Image.fromarray(img[y : y + h, x : x + w, :]))
 
 
 if __name__ == "__main__":
